f21cf9d6e99a01e8cd8d6c180b1daaf758b58f69
Jared Whalen <jared.m.whalen@gmail.com>
2025-11-24 15:08:56 -0500

    update github pages

---
+++ docs/_app/immutable/chunks/CCdaVFPR.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_ohaoql?.base??"",a=globalThis.__sveltekit_ohaoql?.assets??s;export{a,s as b};
--- docs/_app/immutable/chunks/fQZ_eisF.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_dpzdcp?.base??"",a=globalThis.__sveltekit_dpzdcp?.assets??s;export{a,s as b};
